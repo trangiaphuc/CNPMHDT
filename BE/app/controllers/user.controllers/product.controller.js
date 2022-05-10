d8e1b5903a1e8d14290f9dea707ffa8b60194940
Trần Gia Phúc <76146929+trangiaphuc@users.noreply.github.com>
2022-05-10 10:37:48 +0700

    customize response

--- BE/app/controllers/user.controllers/product.controller.js
+++ BE/app/controllers/user.controllers/product.controller.js
@@ -27,6 +27,9 @@ exports.getProductModel = (req, res) => {
                         ],
                     }).then((product) => {
                         const productImage = product.image;
+                        const brand = product.brand;
+                        const quantityUnit = product.quantity;
+
                         if (productImage) {
                             const image = fs.readFileSync(__basedir + productImage.imageUri);
 
@@ -36,7 +39,12 @@ exports.getProductModel = (req, res) => {
 
                             product.setDataValue("productImage", base64ProductImage);
 
+                            const brandName = brand.brandName;
+                            const quantityUnitName = quantityUnit.quantityUnitName;
+
                             model.setDataValue("productModel", product);
+                            model.setDataValue("productBrand", brandName);
+                            model.setDataValue("productQuantity", quantityUnitName);
 
                             if (modelList.indexOf(model) == modelList.length - 1) {
                                 res.status(200).send({ result: modelList });
@@ -65,14 +73,26 @@ exports.getProductWithModel = (req, res) => {
             const productColorList = [];
             productsList.forEach((product) => {
                 const productImage = product.image;
+                const brand = product.brand;
+                const productColor = product.productColor;
+                const quantityUnit = product.quantityUnit;
+
                 if (productImage) {
                     const image = fs.readFileSync(__basedir + productImage.imageUri);
 
                     var base64ProductImage =
                         "data:image/png;base64," + Buffer.from(image).toString("base64");
-                    const productColor = product.productColor;
                     productColorList.push(productColor);
 
+                    const brandName = brand.brandName;
+                    const quantityUnitName = quantityUnit.quantityUnitName;
+                    const colorName = productColor.colorName;
+                    const colorCode = productColor.colorCode;
+
+                    product.setDataValue("productBrand", brandName);
+                    product.setDataValue("productQuantity", quantityUnitName);
+                    product.setDataValue("productColorName", colorName);
+                    product.setDataValue("productColorCode", colorCode);
                     product.setDataValue("productImage", base64ProductImage);
 
                     if (productsList.indexOf(product) == productsList.length - 1) {
@@ -80,12 +100,10 @@ exports.getProductWithModel = (req, res) => {
                         // result.setDataValue("productList", productsList);
                         // result.setDataValue("productColorList", productColorList);
 
-                        res
-                            .status(200)
-                            .send({
-                                productList: productsList,
-                                productColorList: productColorList,
-                            });
+                        res.status(200).send({
+                            productList: productsList,
+                            productColorList: productColorList,
+                        });
                     }
                 }
             });
